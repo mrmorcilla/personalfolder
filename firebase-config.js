ee2f85024553ea4f806203e9274880b0554e8a4f
Andrés <andromedamorillof@gmail.com>
2025-06-17 19:27:08 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYJxA5WeIDms43EAqimDKIo5v1VLy6aBM",
+  authDomain: "personalfolder-7defa.firebaseapp.com",
+  databaseURL: "https://personalfolder-7defa-default-rtdb.firebaseio.com",
+  projectId: "personalfolder-7defa",
+  storageBucket: "personalfolder-7defa.appspot.com",
+  messagingSenderId: "811880952527",
+  appId: "1:811880952527:web:a72b613cbd84924651bdba"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
